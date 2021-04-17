d4d18e67da720bc7011dc99a70b33183360a8bcd
Konstatin Bogdanov <>
2021-04-17 18:57:59 +0300

    up abstraction version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["CoreDataService"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/bogdanovkv/DatabaseAbstraction", from: "1.0.2")
+		.package(url: "https://github.com/bogdanovkv/DatabaseAbstraction", from: "1.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
